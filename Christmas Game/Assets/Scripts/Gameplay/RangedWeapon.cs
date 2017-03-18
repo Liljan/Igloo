df7ca2e562d0380598cc570bb69bbbfdc5aea365
Liljan <marcus.t.lilja@gmail.com>
2017-03-18 17:16:09 +0100

    Can shoot projectiles from gun.

--- Christmas Game/Assets/Scripts/Gameplay/RangedWeapon.cs
+++ Christmas Game/Assets/Scripts/Gameplay/RangedWeapon.cs
@@ -4,32 +4,51 @@ using UnityEngine;
 
 public class RangedWeapon : MonoBehaviour
 {
+    public Sprite sprite;
+    public SpriteRenderer WEAPON_SPRITE_RENDERER;
+    public Transform FIRE_POINT;
 
-    public float aimThreshold = 0.2f;
+    public GameObject BULLET;
+
+    public float AIM_THRESHOLD = 0.2f;
+
+    float timer = 0.0f;
 
     // Use this for initialization
     void Start()
     {
-
+        WEAPON_SPRITE_RENDERER.sprite = sprite;
     }
 
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxis("RIGHT_STICK_HORIZONTAL");
-        float y = -Input.GetAxis("RIGHT_STICK_VERTICAL");
+        float y = Input.GetAxis("RIGHT_STICK_VERTICAL");
 
-         float aimAngle = 0.0f;
+        float aimAngle = 0.0f;
 
-            if (Mathf.Abs(x) < aimThreshold)
-                x = 0.0f;
-            if (Mathf.Abs(y) < aimThreshold)
-                y = 0.0f;
+        if (Mathf.Abs(x) < AIM_THRESHOLD)
+            x = 0.0f;
+        if (Mathf.Abs(y) < AIM_THRESHOLD)
+            y = 0.0f;
 
-            if (x != 0.0f || y != 0.0f)
-                aimAngle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
-
+        if (x != 0.0f || y != 0.0f)
+        {
+            aimAngle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
             this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, aimAngle);
+        }
+
+        if (Input.GetAxis("RIGHT_TRIGGER") > 0.0f && timer <= 0.0f)
+        {
+            GameObject obj = Instantiate(BULLET, FIRE_POINT.position, transform.rotation);
+            obj.GetComponent<Projectile>().Initiate(0); // instantiate with ID 0
+
+            timer = 0.2f;
+        }
+
+        timer -= Time.deltaTime;
+
 
     }
 }
--- Christmas Game/Assets/Scripts/Projectile.cs
+++ Christmas Game/Assets/Scripts/Projectile.cs
@@ -7,20 +7,22 @@ public class Projectile : Attack
 {
     public GameObject PREFAB_PARTICLES;
 
-    public Vector2 speed;
+    public float speed = 15;
     private Rigidbody2D rb2b;
 
     // Use this for initialization
     void Start()
     {
         rb2b = this.gameObject.GetComponent<Rigidbody2D>();
-        rb2b.velocity = new Vector2(speed.x * transform.localScale.x, speed.y * transform.localScale.y);
+        float angle = transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
+        Debug.Log(angle);
+        rb2b.velocity = new Vector2(speed * Mathf.Cos(angle), speed * Mathf.Sin(angle));
     }
 
     public override void HitPlayerEffect()
     {
         GameObject obj = Instantiate(PREFAB_PARTICLES, transform.position, transform.rotation);
-        obj.transform.localScale = transform.localScale;
+        //obj.transform.localScale = transform.localScale;
         Destroy(this.gameObject);
     }
 }

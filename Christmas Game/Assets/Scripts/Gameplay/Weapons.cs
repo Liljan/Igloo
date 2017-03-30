100e803366b0b3ab6eb6fd20c28e594e4d54a592
Liljan <marcus.t.lilja@gmail.com>
2017-03-30 22:09:54 +0200

    Switch weapons code.

--- Christmas Game/Assets/Scripts/Gameplay/Weapons.cs
+++ Christmas Game/Assets/Scripts/Gameplay/Weapons.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Weapons : MonoBehaviour
 {
-
+	public GameObject[] weapons;
+	private int activeWeapon = 0;
     public float AIM_THRESHOLD = 0.2f;
+
+	private bool isDpadPressed = false;
 
     // Use this for initialization
     void Start()
     {
-
+		weapons[activeWeapon].SetActive(true);
     }
 
     // Update is called once per frame
@@ -32,5 +35,59 @@ public class Weapons : MonoBehaviour
             aimAngle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
             this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, aimAngle);
         }
+
+		float dpad = Input.GetAxis("TOGGLE_WEAPON");
+
+		if (dpad != 0.0f) {
+
+			if (!isDpadPressed) {
+
+				if (dpad == 1.0f) {
+					ToggleWeaponForward ();
+				} else if (dpad == -1.0f) {
+					ToggleWeaponBackward ();
+				}
+
+				isDpadPressed = true;
+			}
+		}
+		else
+		{
+			isDpadPressed = false;
+		}
     }
+
+	private void ToggleWeaponForward() {
+
+		/**
+		 * Toggles weapons forwards
+		 */
+
+		weapons[activeWeapon].SetActive(false);
+
+		if (activeWeapon < weapons.Length - 1) {
+			activeWeapon++;
+		} else {
+			activeWeapon = 0;
+		}
+
+		weapons[activeWeapon].SetActive(true);
+	}
+
+	private void ToggleWeaponBackward() {
+
+		/**
+		 * Toggles weapons backwards
+		 */
+
+		weapons[activeWeapon].SetActive(false);
+
+		if (activeWeapon > 0) {
+			activeWeapon--;
+		} else {
+			activeWeapon = weapons.Length - 1;
+		}
+
+		weapons[activeWeapon].SetActive(true);
+	}
 }

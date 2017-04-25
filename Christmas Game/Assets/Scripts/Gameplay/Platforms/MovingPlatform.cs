d7c5bf1c6bd260500a4be40dc6d5ae473ee8ceba
Liljan <marcus.t.lilja@gmail.com>
2017-04-25 20:08:50 +0200

    Moving platform.

---
+++ Christmas Game/Assets/Scripts/Gameplay/Platforms/MovingPlatform.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform[] points;
+    private int currentTarget = 0;
+
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, points[currentTarget].position, speed * Time.deltaTime);
+
+        if (transform.position == points[currentTarget].position)
+        {
+            ++currentTarget;
+            if (currentTarget == points.Length)
+            {
+                currentTarget = 0;
+            }
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = this.transform;
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}

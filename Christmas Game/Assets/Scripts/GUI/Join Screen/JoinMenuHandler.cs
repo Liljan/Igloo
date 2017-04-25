d2fa367c4d0aaeb441a210b45dfbe337e7d5a788
Liljan <marcus.t.lilja@gmail.com>
2017-04-25 22:30:45 +0200

    Started on custom menu items.

---
+++ Christmas Game/Assets/Scripts/GUI/Join Screen/JoinMenuHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinMenuHandler : MonoBehaviour
+{
+    //public JoystickMenuItem[] menuItems;
+
+    public List<JoystickMenuItem> menuItems;
+    public int playerID;
+
+    private int selectedItem = 0;
+
+    // Use this for initialization
+    public void Awake()
+    {
+
+    }
+
+    public void Start()
+    {
+        menuItems[selectedItem].SetSelected(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            SwitchLeft();
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            SwitchRight();
+    }
+
+    private void SwitchRight()
+    {
+        menuItems[selectedItem].SetSelected(false);
+        selectedItem++;
+        if (selectedItem == menuItems.Count)
+            selectedItem = 0;
+
+        menuItems[selectedItem].SetSelected(true);
+    }
+
+    private void SwitchLeft()
+    {
+        menuItems[selectedItem].SetSelected(false);
+        selectedItem--;
+        if (selectedItem < 0)
+            selectedItem = menuItems.Count - 1;
+
+        menuItems[selectedItem].SetSelected(true);
+    }
+
+
+}
---
+++ Christmas Game/Assets/Scripts/GUI/Join Screen/JoystickMenuButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class JoystickMenuButton : JoystickMenuItem
+{
+    public Text text;
+    public string innerText;
+
+    public void Awake()
+    {
+        text.text = innerText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void SetSelected(bool isSelected)
+    {
+        if (isSelected)
+        {
+            text.text = "<" + innerText + ">";
+        }
+        else
+        {
+            text.text = innerText;
+        }
+    }
+}
